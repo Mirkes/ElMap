cf4c7c3ebb48d6c31857546f55642333cea9af70
Evgeny Mirkes <em322@le.ac.uk>
2018-06-13 17:38:24 +0100

    New drawing procedures
    Drawing of different points was added to draw map.

--- drawMap.m
+++ drawMap.m
@@ -1,27 +1,69 @@
-function drawMap( map, data )
+function drawMap( map, data, classes, markColour, markShape, markSize )
 %drawMap create figure and draw data and maps. If data dimension (number of
 %columns) is less than 3 then original data coordinate system is used. If
 %data dimension is greater than 3 then projection into space of the first
 %three PCs is used for drowing.
 %   map is object of class MapGeometry (descendant of this class).
-%   data is matrix of data points. each row is one point.
+%   data is n-by-dim matrix of data points. Each row is one point.
+%   classes is n-by-1 vector of class labels for points. Each label is
+%       positive integer number which is the index of cell array with
+%       marker descriptions. Marker descriptions can be specified by user
+%       in the arguments markColour, markShape, markSize. Otherwise
+%       standard markers are used.
+%   markColour is K-by-1 vector of standard Matlab colours ('r', 'g', 'b',
+%       'y', 'm', 'c', 'w', 'k'). Value K is number of defined markers. If
+%       markColour is omitted then 'b' is used for all markers.
+%   markShape is K-by-1 vector of standard Matlab marker shapes ('o', '+',
+%       '*', '.', 'x', 's', 'd', '^', 'v', '<', '>', 'p', 'h'). Value K is
+%       number of defined markers. If markShape is omitted then 's' is used
+%       for all markers.
+%   markSize is K-by-1 vector of positive numbers. Value K is number of
+%       defined markers. If markSize is omitted then 6 is used 
+%       for all markers.
 
+    %Get data dimension
+    [N, dim] = size(data);
+
+    if nargin<3 || isempty(classes)
+        classes = ones(N, 1);
+        cls = 1;
+    else
+        cls = unique(classes);
+    end
+    
+    nCls = length(cls);
+    
+    if nargin < 4 || isempty(markColour)
+        markColour = repmat('b', nCls, 1);
+    end
+
+    if nargin < 5 || isempty(markShape)
+        markShape = repmat('s', nCls, 1);
+    end
+    
+    if nargin < 6 || isempty(markSize)
+        markSize = repmat(6, nCls, 1);
+    end
+    
     %Create figure
     figure;
-    axis equal;
     %Get map coordinates
     maps = map.getMappedCoordinates;
     %get map links
     links = map.getLinks;
-    %Get data dimension
-    dim = size(data,2);
     if dim>3 % Dimension is greater than 3 and we need to use PCs
         % Calculate three first PCs and project data onto PCs
         [~, ~, V] = svds(data, 3);
         data = data * V;
         %Draw data
-        plot3(data(:, 1), data(:, 2), data(:, 3), 'sb', 'MarkerFaceColor','b');
-        hold on
+        for k = 1:nCls
+            ind = classes == cls(k);
+            plot3(data(ind, 1), data(ind, 2), data(ind, 3),...
+                [markColour(k), markShape(k)],...
+                'MarkerFaceColor', markColour(k),...
+                'MarkerSize', markSize(k));
+            hold on
+        end
         %Draw edges
         %Prepare arrays
         maps = maps * V;
@@ -32,11 +74,18 @@ function drawMap( map, data )
         plot3(X, Y, Z, 'r', 'LineWidth', 2);
         %Draw map nodes
         plot3(maps(:, 1), maps(:, 2), maps(:, 3), 'ro', 'MarkerFaceColor', 'r');        
+        axis equal;
     elseif dim==3
         %3d data
         %Draw data
-        plot3(data(:, 1), data(:, 2), data(:, 3), 'sb', 'MarkerFaceColor','b');
-        hold on
+        for k = 1:nCls
+            ind = classes == cls(k);
+            plot3(data(ind, 1), data(ind, 2), data(ind, 3),...
+                [markColour(k), markShape(k)],...
+                'MarkerFaceColor', markColour(k),...
+                'MarkerSize', markSize(k));
+            hold on
+        end
         %Draw edges
         %Prepare arrays
         X=[maps(links(:,1), 1)';maps(links(:,2), 1)'];
@@ -46,11 +95,18 @@ function drawMap( map, data )
         plot3(X, Y, Z, 'r', 'LineWidth', 2);
         %Draw map nodes
         plot3(maps(:, 1), maps(:, 2), maps(:, 3), 'ro', 'MarkerFaceColor', 'r');        
+        axis equal;
     elseif dim==2
         %two dimensional data
         %Draw data
-        plot(data(:,1),data(:,2),'sb','MarkerFaceColor','b');
-        hold on
+        for k = 1:nCls
+            ind = classes == cls(k);
+            plot(data(ind, 1), data(ind, 2),...
+                [markColour(k), markShape(k)],...
+                'MarkerFaceColor', markColour(k),...
+                'MarkerSize', markSize(k));
+            hold on
+        end
         %Draw edges
         %Prepare arrays
         X=[maps(links(:,1),1)';maps(links(:,2),1)'];
@@ -59,11 +115,18 @@ function drawMap( map, data )
         plot(X,Y,'r','LineWidth',2);
         %Draw map nodes
         plot(maps(:,1),maps(:,2),'ro','MarkerFaceColor','r');        
+        axis equal;
     else
         %one dimensional data
         %Draw data
-        plot(data(:,1),0,'sb','MarkerFaceColor','b');
-        hold on
+        for k = 1:nCls
+            ind = classes == cls(k);
+            plot(data(ind, 1), 0,...
+                [markColour(k), markShape(k)],...
+                'MarkerFaceColor', markColour(k),...
+                'MarkerSize', markSize(k));
+            hold on
+        end
         %Draw edges
         %Prepare arrays
         X=[maps(links(:,1),1)';maps(links(:,2),1)'];
--- tests/tester.m
+++ tests/tester.m
@@ -1,11 +1,13 @@
-what = 11;
+what = 12;
 
 % What is parameter to select test
 % 0 - generate data for test
 % 1 - Generate randomly distorted arc of circle
 % 2 - Create two arcs with shift of z coordinate
-% 3 - Create two arcs with shift of x and y coordinates
+% 3 - Create two arcs with shift of x coordinate
 % 4 - Create fragment of sphere
+% 5 - Create two fragments of sphere with shift
+
 % 11 - test oneDMap
 % 12 - test rect2DMap
 % 13 - test tri2DMap
@@ -28,7 +30,12 @@ switch what
     case 4
         data = createTestSurf(3, 200, pi / 4, 20, 1);
         save('data3DSurf.mat', 'data', '-v7.3');
-        
+    case 5
+        data = createTestSurf(3, 500, pi / 4, 20, 1);
+        data(1:5:500, 3) = data(1:5:500, 3) + 5;
+        classes = ones(500, 1);
+        classes(1:5:500) = classes(1:5:500) + 1;
+        save('data3DSurf2.mat', 'data', 'classes', '-v7.3');
     case 11
         map = OneDMap(10);
         init(map, data, 'pci');
@@ -44,10 +51,22 @@ switch what
     case 12
         map = rect2DMap(10, 10);
         init(map, data, 'pci');
-        drawMap(map, data);
+        if exist('classes', 'var') == 1
+            drawMap(map, data, classes, ['b','g']);
+        else
+            drawMap(map, data);
+        end
         drawMapInt(map, data);
-        EM(map, data, 'stretch', 0.001, 'bend', 0.01);
-        drawMap(map, data);
+%         EM(map, data, 'stretch', 0.01, 'bend', 0.1);
+%         EM(map, data, 'stretch', 0.0001, 'bend', 0.1, 'potential', @LLog,...
+%             'Number_of_intervals', 5);
+        EM(map, data, 'stretch', 0.01, 'bend', 0.1, 'potential', @L2,...
+            'Number_of_intervals', 2, 'intshrinkage', 0.7);
+        if exist('classes', 'var') == 1
+            drawMap(map, data, classes, ['b','g']);
+        else
+            drawMap(map, data);
+        end
         drawMapInt(map, data);
     case 13
         map = tri2DMap(4, 4);
