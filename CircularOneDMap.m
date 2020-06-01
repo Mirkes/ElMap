ad26780d5122fee9adeae3eb729e8d63175f45b4
Evgeny Mirkes <em322@le.ac.uk>
2020-06-01 15:24:37 +0100

    Cosmetic correction of circular map

--- CircularOneDMap.m
+++ CircularOneDMap.m
@@ -9,21 +9,21 @@ classdef CircularOneDMap < MapGeometry
             % Store size
             map.sizes = N;
             % Calculate internal coordinates of nodes
-	    x = (1:N)'/N;
+            x = (1:N)' / N;
             map.internal = [cos(2*pi*x), sin(2*pi*x)];
             % Form array of links
             map.links = [1:N-1;2:N]';
-	    map.links = [map.links;[N,1]]
+            map.links = [map.links;[N,1]]
             % Form array of ribs
             map.ribs = [1:N-2;2:N-1;3:N]';
-	    map.ribs = [map.ribs;[N-1, N, 1]];
-	    map.ribs = [map.ribs;[N, 1, 2]]	
+            map.ribs = [map.ribs;[N-1, N, 1]];
+            map.ribs = [map.ribs;[N, 1, 2]]
             % Set mapped coordinates to empty set
             map.mapped = [];
         end
 
         function newMap = extendPrim(map)
-	    newMap = map;
+            newMap = map;
         end
 
                 
